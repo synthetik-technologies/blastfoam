6ab7026bb99232f65804883919213c6d0abe71ea
Jeff Heylmun <heylmun@synthetik-technologies.com>
2020-04-20 12:08:37 -0500

    DEV: Added utility to merge probes from multiple start times Useful when cases need to be restarted, but continuous probes are needed

---
+++ applications/utilities/mergeProbes/mergeProbes.C
@@ -0,0 +1,159 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2019 Synthetik Applied Technologies
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Utility to create symbolic link to post processing vtk files for easier
+    viewing in paraview
+
+\*---------------------------------------------------------------------------*/
+
+#include "argList.H"
+#include "Time.H"
+#include "Istream.H"
+#include "IFstream.H"
+#include "OFstream.H"
+#include "SortableList.H"
+
+using namespace Foam;
+
+int main(int argc, char *argv[])
+{
+    argList::addBoolOption
+    (
+        "force",
+        "Remove VTK directory if currently present"
+    );
+    argList::addOption
+    (
+        "probeName",
+        "Name of probe to merge"
+    );
+    argList::addOption
+    (
+        "probeDict",
+        "Name of probe dictionary"
+    );
+
+    #include "setRootCase.H"
+
+    bool force(args.optionFound("force"));
+    wordList probeNames(args.optionLookupOrDefault("probeNames", wordList()));
+    word probeDirName(args.option("probeDict"));
+
+    // Create the processor databases
+    fileName postProcessDir
+    (
+        args.caseName()/fileName(word("postProcessing"))
+    );
+    fileName probesDir(args.rootPath()/postProcessDir/probeDirName);
+    wordList times(readDir(probesDir, fileType::directory));
+    SortableList<scalar> sTimes(times.size());
+
+    // Sort times
+    {
+        forAll(sTimes, ti)
+        {
+            IStringStream is(times[ti]);
+            sTimes[ti] = readScalar(is);
+        }
+        sTimes.sort();
+        wordList oldTimes(times);
+        forAll(sTimes, ti)
+        {
+            times[ti] = oldTimes[sTimes.indices()[ti]];
+        }
+    }
+    sTimes.append(great);
+
+    // Get list of probes
+    if (!args.optionFound("probeNames"))
+    {
+        fileName probeDir(probesDir/times[0]);
+        probeNames = wordList(readDir(probeDir, fileType::file));
+    }
+    if (!force)
+    {
+        wordList writtenProbes;
+        forAll(probeNames, probei)
+        {
+            if (!isFile(probesDir/probeNames[probei]))
+            {
+                writtenProbes.append(probeNames[probei]);
+            }
+            else
+            {
+                WarningInFunction
+                    << probeNames[probei] << " already found. Skipping probe."
+                    << endl;
+            }
+        }
+    }
+
+    Info<< "Merging probes: " << nl
+        << probeNames << endl;
+
+    // Create outputs
+    PtrList<OFstream> outputs(probeNames.size());
+    forAll(outputs, probei)
+    {
+        outputs.set(probei, new OFstream(probesDir/probeNames[probei]));
+    }
+
+    scalar nextTime = 0.0;
+    forAll(times, timei)
+    {
+        nextTime = sTimes[timei + 1];
+        fileName probeDir(probesDir/times[timei]);
+
+        forAll(probeNames, probei)
+        {
+            IFstream stream(probeDir/probeNames[probei]);
+
+            while (stream.good())
+            {
+                string line;
+                stream.getLine(line);
+                char firstChar(line[0]);
+
+                if (firstChar == '#')
+                {
+                    continue;
+                }
+
+                IStringStream is(line);
+                scalar t = readScalar(is);
+
+                if (t < nextTime)
+                {
+                    outputs[probei] << word(line) << nl;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    Info<< nl << "Done." << endl;
+}
