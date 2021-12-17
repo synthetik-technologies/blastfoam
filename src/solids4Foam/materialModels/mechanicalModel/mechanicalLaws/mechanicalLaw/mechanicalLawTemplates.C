477dd8a73a1c7594958689983dfef1e1f1d742dc
Jeff Heylmun <heylmun@synthetik-technologies.com>
2021-12-17 17:33:07 -0600

    DEV: Added missing file

---
+++ src/solids4Foam/materialModels/mechanicalModel/mechanicalLaws/mechanicalLaw/mechanicalLawTemplates.C
@@ -0,0 +1,388 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     |
+    \\  /    A nd           | For copyright notice see file Copyright
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+
+InClass
+    Foam::mechanicalLaw
+
+\*---------------------------------------------------------------------------*/
+
+#include "mechanicalLaw.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+template<class Type, template<class> class PatchField, class GeoMesh>
+autoPtr<GeometricField<Type, PatchField, GeoMesh>> mechanicalLaw::makeField
+(
+    const word& name,
+    const dimensioned<Type>& dt,
+    const IOobject::readOption rOpt,
+    const IOobject::writeOption wOpt
+) const
+{
+    return autoPtr<GeometricField<Type, PatchField, GeoMesh>>
+    (
+        new GeometricField<Type, PatchField, GeoMesh>
+        (
+            IOobject
+            (
+                name,
+                mesh_.time().timeName(mesh_.time().startTime().value()),
+                mesh_,
+                rOpt,
+                wOpt
+            ),
+            mesh_,
+            dt
+        )
+    );
+}
+
+
+template<class Type, template<class> class PatchField, class GeoMesh>
+autoPtr<GeometricField<Type, PatchField, GeoMesh>>
+mechanicalLaw::makeTypeField
+(
+    const word& name,
+    const dimensioned<Type>& dt,
+    const IOobject::readOption rOpt,
+    const IOobject::writeOption wOpt
+) const
+{
+    return autoPtr<GeometricField<Type, PatchField, GeoMesh>>
+    (
+        new GeometricField<Type, PatchField, GeoMesh>
+        (
+            IOobject
+            (
+                name_ + ":" + name,
+                mesh_.time().timeName(mesh_.time().startTime().value()),
+                mesh_,
+                rOpt,
+                wOpt
+            ),
+            mesh_,
+            dt
+        )
+    );
+}
+
+
+template<class Type, template<class> class PatchField, class GeoMesh>
+autoPtr<GeometricField<Type, PatchField, GeoMesh>>
+mechanicalLaw::makeField
+(
+    const IOobject& io,
+    const dimensioned<Type>& dt
+) const
+{
+    return autoPtr<GeometricField<Type, PatchField, GeoMesh>>
+    (
+        new GeometricField<Type, PatchField, GeoMesh>
+        (
+            io,
+            mesh_,
+            dt
+        )
+    );
+}
+
+
+template<class FieldType, class CoeffType>
+void mechanicalLaw::replacePlaneStress
+(
+    FieldType& epsilon,
+    const CoeffType& coeff,
+    const FieldType& sigma,
+    const FieldType& epsilonP
+) const
+{
+    if (usePlaneStress_)
+    {
+        epsilon.replace
+        (
+            planeStressDir_,
+           -coeff
+           *(
+               sigma.component(nonPlaneStressDirs_[0])
+             + sigma.component(nonPlaneStressDirs_[1])
+            )
+          - (
+                epsilonP.component(nonPlaneStressDirs_[0])
+              + epsilonP.component(nonPlaneStressDirs_[1])
+            )
+        );
+    }
+}
+
+
+template<class FieldType, class CoeffType>
+void mechanicalLaw::replacePlaneStress
+(
+    FieldType& epsilon,
+    const CoeffType& coeff,
+    const FieldType& sigma
+) const
+{
+    if (usePlaneStress_)
+    {
+        epsilon.replace
+        (
+            planeStressDir_,
+           -coeff
+           *(
+               sigma.component(nonPlaneStressDirs_[0])
+             + sigma.component(nonPlaneStressDirs_[1])
+            )
+        );
+    }
+}
+
+
+template<template<class> class PatchField, class Mesh, class CoeffType>
+void mechanicalLaw::updateEpsilon
+(
+    GeometricField<symmTensor, PatchField, Mesh>& epsilon,
+    const CoeffType& coeff,
+    const GeometricField<symmTensor, PatchField, Mesh>& sigma,
+    const GeometricField<symmTensor, PatchField, Mesh>& epsilonP
+) const
+{
+    word ext = word::null;
+    if (epsilon.size() == mesh_.nInternalFaces())
+    {
+        ext = "f";
+    }
+    // Calculate total strain
+    if (incremental())
+    {
+        // Lookup gradient of displacement increment
+        const GeometricField<tensor, PatchField, Mesh>& gradDD =
+            mesh().lookupObject
+            <
+                GeometricField<tensor, PatchField, Mesh>
+            >("grad(DD)" + ext);
+
+        epsilon = epsilon.oldTime() + symm(gradDD);
+    }
+    else
+    {
+        // Lookup gradient of displacement
+        const GeometricField<tensor, PatchField, Mesh>& gradD =
+            mesh().lookupObject
+            <
+                GeometricField<tensor, PatchField, Mesh>
+            >("grad(D)" + ext);
+        epsilon = symm(gradD);
+    }
+    replacePlaneStress(epsilon, coeff, sigma, epsilonP);
+}
+
+
+template<template<class> class PatchField, class Mesh, class CoeffType>
+void mechanicalLaw::updateEpsilon
+(
+    GeometricField<symmTensor, PatchField, Mesh>& epsilon,
+    const CoeffType& coeff,
+    const GeometricField<symmTensor, PatchField, Mesh>& sigma
+) const
+{
+    word ext = word::null;
+    if (epsilon.size() == mesh_.nInternalFaces())
+    {
+        ext = "f";
+    }
+
+    // Calculate total strain
+    if (incremental())
+    {
+        // Lookup gradient of displacement increment
+        const GeometricField<tensor, PatchField, Mesh>& gradDD =
+            mesh().lookupObject
+            <
+                GeometricField<tensor, PatchField, Mesh>
+            >("grad(DD)" + ext);
+
+        epsilon = epsilon.oldTime() + symm(gradDD);
+    }
+    else
+    {
+        // Lookup gradient of displacement
+        const GeometricField<tensor, PatchField, Mesh>& gradD =
+            mesh().lookupObject
+            <
+                GeometricField<tensor, PatchField, Mesh>
+            >("grad(D)" + ext);
+
+        epsilon = symm(gradD);
+    }
+    replacePlaneStress(epsilon, coeff, sigma);
+}
+
+
+template<template<class> class PatchField, class Mesh, class CoeffType>
+void mechanicalLaw::updateEpsilon
+(
+    GeometricField<symmTensor, PatchField, Mesh>& epsilon,
+    const tmp<CoeffType>& coeff,
+    const GeometricField<symmTensor, PatchField, Mesh>& sigma
+) const
+{
+    updateEpsilon(epsilon, coeff(), sigma);
+}
+
+
+template<template<class> class PatchField, class Mesh, class CoeffType>
+void mechanicalLaw::updateEpsilon
+(
+    GeometricField<symmTensor, PatchField, Mesh>& epsilon,
+    const tmp<CoeffType>& coeff,
+    const GeometricField<symmTensor, PatchField, Mesh>& sigma,
+    const GeometricField<symmTensor, PatchField, Mesh>& epsilonP
+) const
+{
+    updateEpsilon(epsilon, coeff(), sigma, epsilonP);
+}
+
+
+template<template<class> class PatchField, class Mesh, class CoeffType>
+tmp<GeometricField<symmTensor, PatchField, Mesh>>
+mechanicalLaw::calcEpsilon
+(
+    const CoeffType& coeff,
+    const GeometricField<symmTensor, PatchField, Mesh>& sigma,
+    const GeometricField<symmTensor, PatchField, Mesh>& epsilonP
+) const
+{
+    tmp<GeometricField<symmTensor, PatchField, Mesh>> tepsilon
+    (
+        calcEpsilon(sigma)
+    );
+    replacePlaneStress(tepsilon.ref(), coeff, sigma, epsilonP);
+    return tepsilon;
+}
+
+
+template<template<class> class PatchField, class Mesh, class CoeffType>
+tmp<GeometricField<symmTensor, PatchField, Mesh>>
+mechanicalLaw::calcEpsilon
+(
+    const CoeffType& coeff,
+    const GeometricField<symmTensor, PatchField, Mesh>& sigma
+) const
+{
+    tmp<GeometricField<symmTensor, PatchField, Mesh>> tepsilon
+    (
+        calcEpsilon(sigma)
+    );
+    replacePlaneStress(tepsilon.ref(), coeff, sigma);
+    return tepsilon;
+}
+
+
+template<template<class> class PatchField, class Mesh, class CoeffType>
+tmp<GeometricField<symmTensor, PatchField, Mesh>>
+mechanicalLaw::calcEpsilon
+(
+    const tmp<CoeffType>& coeff,
+    const GeometricField<symmTensor, PatchField, Mesh>& sigma,
+    const GeometricField<symmTensor, PatchField, Mesh>& epsilonP
+) const
+{
+    return calcEpsilon(coeff(), sigma, epsilonP);
+}
+
+
+template<template<class> class PatchField, class Mesh, class CoeffType>
+tmp<GeometricField<symmTensor, PatchField, Mesh>>
+mechanicalLaw::calcEpsilon
+(
+    const tmp<CoeffType>& coeff,
+    const GeometricField<symmTensor, PatchField, Mesh>& sigma
+) const
+{
+    return calcEpsilon(coeff(), sigma);
+}
+
+
+template<class CoeffType>
+tmp<Field<symmTensor>> mechanicalLaw::calcEpsilon
+(
+    const CoeffType& coeff,
+    const Field<symmTensor>& sigma,
+    const Field<symmTensor>& epsilonP,
+    const label patchi
+) const
+{
+    tmp<Field<symmTensor>> tepsilon(calcEpsilon(patchi));
+    replacePlaneStress(tepsilon.ref(), coeff, sigma, epsilonP);
+    return tepsilon;
+}
+
+
+template<class CoeffType>
+tmp<Field<symmTensor>> mechanicalLaw::calcEpsilon
+(
+    const CoeffType& coeff,
+    const Field<symmTensor>& sigma,
+    const label patchi
+) const
+{
+    tmp<Field<symmTensor>> tepsilon(calcEpsilon(patchi));
+    replacePlaneStress(tepsilon.ref(), coeff, sigma);
+    return tepsilon;
+}
+
+
+template<class CoeffType>
+tmp<Field<symmTensor>> mechanicalLaw::calcEpsilon
+(
+    const tmp<CoeffType>& coeff,
+    const Field<symmTensor>& sigma,
+    const Field<symmTensor>& epsilonP,
+    const label patchi
+) const
+{
+    return calcEpsilon(coeff(), sigma, patchi, epsilonP);
+}
+
+
+template<class CoeffType>
+tmp<Field<symmTensor>> mechanicalLaw::calcEpsilon
+(
+    const tmp<CoeffType>& coeff,
+    const Field<symmTensor>& sigma,
+    const label patchi
+) const
+{
+    return calcEpsilon(coeff(), sigma, patchi);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
