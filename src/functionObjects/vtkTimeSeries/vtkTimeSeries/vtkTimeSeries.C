fb9d1745e981b3513dd2057c1cde1d86944a0ba7
Jeff Heylmun <heylmun@synthetik-technologies.com>
2022-05-09 13:06:56 -0500

    DEV: Added vtkTimeSeries surfaceWriter

---
+++ src/functionObjects/vtkTimeSeries/vtkTimeSeries/vtkTimeSeries.C
@@ -0,0 +1,120 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2021
+     \\/     M anipulation  | Synthetik Applied Technologies
+-------------------------------------------------------------------------------
+License
+    This file is derivative work of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "vtkTimeSeries.H"
+#include "objectRegistry.H"
+#include "Time.H"
+#include "OFstream.H"
+#include "List.H"
+#include "OSspecific.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(vtkTimeSeries, 0);
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::vtkTimeSeries::vtkTimeSeries(const fileName& path, const label nRemove)
+:
+    nRemove_(nRemove),
+    path_()
+{
+    wordList cmpts(path.components());
+    for (label i = 0; i < cmpts.size() - nRemove_; i++)
+    {
+        path_  +=  '/' + cmpts[i];
+    }
+
+    fileNameList dirs(readDir(path_, fileType::directory, true, false));
+    forAll(dirs, i)
+    {
+        IStringStream is(word(dirs[i]));
+        token t(is);
+        if (t.isNumber())
+        {
+            this->insert(t.number());
+        }
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::vtkTimeSeries::~vtkTimeSeries()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+bool Foam::vtkTimeSeries::writeTimeSeries
+(
+    const fileName& path,
+    const fileName& name
+) const
+{
+
+    OFstream os(path_ / name +".vtk.series");
+    wordList cmpts(path.components());
+    const_cast<scalarHashSet&>
+    (
+        dynamicCast<const scalarHashSet&>(*this)
+    ).insert
+    (
+        readScalar((IStringStream(cmpts[cmpts.size() - nRemove_]))())
+    );
+
+    // Header
+    os
+        << '{' << nl
+        << "  " << string("file-series-version") << " : " << string("1.0") << ',' << nl
+        << "  " << string("files") << " : [";
+
+    scalarList times(this->sortedToc());
+    forAll(times, i)
+    {
+        os  << nl
+            << "    { "
+            << string("name") << " : "
+            << fileName(path / name + ".vtk") << " ,"
+            << string("time") << " : " << times[i]
+            << " }";
+
+        if (i != this->size() - 1)
+        {
+            os << ",";
+        }
+    }
+    os  << nl
+        << "  ]" << nl
+        << "}" << endl;
+
+    return os.good();
+}
+
+
+// ************************************************************************* //
---
+++ src/functionObjects/vtkTimeSeries/vtkTimeSeriesWriter/vtkTimeSeriesSurfaceWriter.C
@@ -0,0 +1,131 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2021 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "vtkTimeSeriesSurfaceWriter.H"
+#include "OFstream.H"
+#include "boolList.H"
+#include "OSspecific.H"
+#include "makeSurfaceWriterMethods.H"
+#include "vtkWritePolyData.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    makeSurfaceWriterType(vtkTimeSeriesSurfaceWriter);
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+template<class Type>
+void Foam::vtkTimeSeriesSurfaceWriter::Write
+(
+    const fileName& outputDir,
+    const fileName& surfaceName,
+    const pointField& points,
+    const faceList& faces,
+    const word& fieldName,
+    const Field<Type>& values,
+    const bool isNodeValues
+) const
+{
+    if (!isDir(outputDir))
+    {
+        mkDir(outputDir);
+    }
+
+    if (!timeSeries_.valid())
+    {
+        timeSeries_.set(new vtkTimeSeries(outputDir, 1));
+    }
+
+    vtkWritePolyData::write
+    (
+        outputDir/fieldName + '_' + surfaceName + ".vtk",
+        "sampleSurface",
+        writeFormat_ == IOstream::BINARY,
+        points,
+        labelList(),
+        edgeList(),
+        faces,
+        fieldName,
+        isNodeValues,
+        values
+    );
+    timeSeries_->writeTimeSeries(outputDir, fieldName + '_' + surfaceName);
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::vtkTimeSeriesSurfaceWriter::vtkTimeSeriesSurfaceWriter
+(
+    const IOstream::streamFormat writeFormat
+)
+:
+    surfaceWriter(writeFormat)
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::vtkTimeSeriesSurfaceWriter::~vtkTimeSeriesSurfaceWriter()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::vtkTimeSeriesSurfaceWriter::write
+(
+    const fileName& outputDir,
+    const fileName& surfaceName,
+    const pointField& points,
+    const faceList& faces
+) const
+{
+    if (!isDir(outputDir))
+    {
+        mkDir(outputDir);
+    }
+
+    vtkWritePolyData::write
+    (
+        outputDir/surfaceName + ".vtk",
+        "sampleSurface",
+        writeFormat_ == IOstream::BINARY,
+        points,
+        labelList(),
+        edgeList(),
+        faces
+    );
+}
+
+
+// Create write methods
+defineSurfaceWriterWriteFields(Foam::vtkTimeSeriesSurfaceWriter);
+
+
+// ************************************************************************* //
