7e51e55cab113c13127e5176d5709887bcbba864
Jacob Hoehler <hoehler@synthetik-technologies.com>
2022-08-25 14:08:01 -0400

    ci: use dch for updating version

--- .updateDeb.sh
+++ .updateDeb.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 version="$1"
-versionString="Version: $1"
+versionString="Version: $1" 
 sed -i "s/Version:.*/$versionString/g" debian/blastfoam/DEBIAN/control
-sed -i -E "s/(blastfoam_)(.*)(_.*)/\1$version\3/g" debian/files
+dch -v $version " "
+# sed -i -E "s/(blastfoam_)(.*)(_.*)/\1$version\3/g" debian/files
