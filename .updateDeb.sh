39686178451c863296ba8e00cc8dc27de9dfba78
Jacob Hoehler <hoehler@synthetik-technologies.com>
2022-08-25 14:22:10 -0400

    ci: re-enable 'files' update

--- .updateDeb.sh
+++ .updateDeb.sh
@@ -3,5 +3,5 @@
 version="$1"
 versionString="Version: $1" 
 sed -i "s/Version:.*/$versionString/g" debian/blastfoam/DEBIAN/control
-dch -v $version " "
-# sed -i -E "s/(blastfoam_)(.*)(_.*)/\1$version\3/g" debian/files
+dch -v $version "Automated Release"
+sed -i -E "s/(blastfoam_)(.*)(_.*)/\1$version\3/g" debian/files
