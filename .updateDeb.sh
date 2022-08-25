729a7c3b6131e557fc90150ee9887c7c60ccb577
Jacob Hoehler <hoehler@synthetik-technologies.com>
2022-08-25 09:46:38 -0400

    ci: update additional files on release

--- .updateDeb.sh
+++ .updateDeb.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-version="Version: $1"
-sed -i "s/Version:.*/$version/g" debian/blastfoam/DEBIAN/control
+version="$1"
+versionString="Version: $1"
+sed -i "s/Version:.*/$versionString/g" debian/blastfoam/DEBIAN/control
+sed -i "s/(blastfoam_)(.*)(_.*)/${1}$version$3/g" debian/files
