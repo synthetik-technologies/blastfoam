0c6a1763480a1d1b67b9ff875f79686c8f7a024e
Jacob Hoehler <hoehler@synthetik-technologies.com>
2022-08-25 13:33:52 -0400

    ci: fix regex for sed

--- .updateDeb.sh
+++ .updateDeb.sh
@@ -3,4 +3,4 @@
 version="$1"
 versionString="Version: $1"
 sed -i "s/Version:.*/$versionString/g" debian/blastfoam/DEBIAN/control
-sed -i "s/(blastfoam_)(.*)(_.*)/${1}$version$3/g" debian/files
+sed -i -E "s/(blastfoam_)(.*)(_.*)/\1$version\3/g" debian/files
