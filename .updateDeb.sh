f544a64588ca98a8692ee0943a4899869ed82620
Jacob Hoehler <hoehler@synthetik-technologies.com>
2022-08-29 11:56:17 -0400

    Revert "ci: debug changelog script"
    This reverts commit ab62ae6e297a9276a66bdf936f56ae07850a02cb.

--- .updateDeb.sh
+++ .updateDeb.sh
@@ -5,8 +5,3 @@ versionString="Version: $1"
 sed -i "s/Version:.*/$versionString/g" debian/blastfoam/DEBIAN/control
 dch -v $version "Automated Release"
 sed -i -E "s/(blastfoam_)(.*)(_.*)/\1$version\3/g" debian/files
-
-echo "Debug"
-cat debian/blastfoam/DEBIAN/control
-cat debian/files
-cat debian/changelog
