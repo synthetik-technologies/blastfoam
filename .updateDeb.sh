79cc0d72690beca9c83a535fe220e49414aa22b6
Jacob Hoehler <hoehler@synthetik-technologies.com>
2022-09-02 13:33:48 -0400

    fix: force version number to change in changelog

--- .updateDeb.sh
+++ .updateDeb.sh
@@ -3,5 +3,5 @@
 version="$1"
 versionString="Version: $1" 
 sed -i "s/Version:.*/$versionString/g" debian/blastfoam/DEBIAN/control
-dch -v $version "Automated Release"
+dch -v -b $version "Automated Release"
 sed -i -E "s/(blastfoam_)(.*)(_.*)/\1$version\3/g" debian/files
