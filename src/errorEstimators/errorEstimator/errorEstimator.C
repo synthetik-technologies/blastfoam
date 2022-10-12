9e20f98ab1227aeb976d600877359ed7d48610c1
Jeff Heylmun <heylmun@synthetik-technologies.com>
2022-10-12 14:47:43 -0500

    DEV: Added blastModels library to Allwmake

--- src/errorEstimators/errorEstimator/errorEstimator.C
+++ src/errorEstimators/errorEstimator/errorEstimator.C
@@ -306,7 +306,7 @@ bool Foam::errorEstimator::writeData(Ostream&) const
         }
         maxLevel.write();
 
-        // if (minDx_ > 0)
+        if (minDx_ > 0)
         {
             const meshSizeObject& mso = meshSizeObject::New(mesh_);
             const_cast<meshSizeObject&>(mso).movePoints();
