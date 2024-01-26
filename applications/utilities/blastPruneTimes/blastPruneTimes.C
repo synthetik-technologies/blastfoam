5f53433c8308a00e407d141e0c0390d833efce7b
Jeff Heylmun <heylmun@synthetik-technologies.com>
2024-01-26 14:28:53 -0600

    DEV: Added blastPruneTimes

---
+++ applications/utilities/blastPruneTimes/blastPruneTimes.C
@@ -0,0 +1,274 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
+-------------------------------------------------------------------------------
+03-02-2023:                 | Modified foamListTimes to allow for selecting
+    Synthetik Applied       | times within an interval and with prescribed
+    Technologie             | spacing
+-------------------------------------------------------------------------------
+License
+    This file is a derived work of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Application
+    blastPruneTimes
+
+Description
+    Select time between a start and end time with a given spacing
+
+\*---------------------------------------------------------------------------*/
+
+#include "argList.H"
+#include "timeSelector.H"
+#include "Time.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+using namespace Foam;
+int main(int argc, char *argv[])
+{
+    writeInfoHeader = false;
+
+    argList::addNote("List times from start : spacing : end");
+    timeSelector::addOptions(true, true);
+    argList::noParallel();
+    argList::addBoolOption
+    (
+        "processor",
+        "list times from processor0/ directory"
+    );
+    argList::addBoolOption
+    (
+        "rm",
+        "remove selected time directories"
+    );
+    argList::addBoolOption
+    (
+        "withFunctionObjects",
+        "execute functionObjects"
+    );
+
+    argList::addBoolOption
+    (
+        "withFunctionObjects",
+        "execute functionObjects"
+    );
+
+    argList::addOption
+    (
+        "startTime",
+        "Start of interval"
+    );
+    argList::addOption
+    (
+        "endTime",
+        "End of interval"
+    );
+    argList::addOption
+    (
+        "dt",
+        "Time spacing"
+    );
+    argList::addOption
+    (
+        "n",
+        "number of times to save"
+    );
+
+    argList::addOption
+    (
+        "tolerance",
+        "tolerance for finding times"
+    );
+
+    argList::addBoolOption
+    (
+        "invert",
+        "select non-interval times"
+    );
+
+
+    #include "addRegionOption.H"
+    #include "setRootCase.H"
+
+    label nProcs = 1;
+    PtrList<Time> databases(1);
+    if (args.optionFound("processor"))
+    {
+        nProcs = fileHandler().nProcs(args.path());
+        if (!nProcs)
+        {
+            FatalErrorInFunction
+                << "Trying to show time for a parallel case, but the case" << nl
+                << "has not been deomcomposed." << endl
+                << abort(FatalError);
+        }
+        databases.setSize(nProcs);
+        forAll(databases, proci)
+        {
+            databases.set
+            (
+                proci,
+                new Time
+                (
+                    Time::controlDictName,
+                    args.rootPath(),
+                    args.caseName()/fileName("processor" + Foam::name(proci))
+                )
+            );
+        }
+    }
+    else
+    {
+        databases.set
+        (
+            0,
+            new Time
+            (
+                Time::controlDictName,
+                args.rootPath(),
+                args.caseName()
+            )
+        );
+    }
+
+    instantList times = timeSelector::select
+    (
+        databases[0].times(),
+        args
+    );
+
+    DynamicList<instant> timesToKeep(times.size());
+    DynamicList<instant> prunedTimes(times.size());
+    const scalar startTime =
+        args.optionLookupOrDefault("startTime", databases[0].startTime().value());
+    const scalar endTime =
+        args.optionLookupOrDefault("endTime", databases[0].endTime().value());
+    scalar dt = endTime - startTime;
+    if (args.optionFound("dt"))
+    {
+        dt = args.optionRead<scalar>("dt");
+    }
+    else if (args.optionFound("n"))
+    {
+        dt /= args.optionRead<scalar>("n");
+    }
+
+    const scalar tolerance = args.optionLookupOrDefault("tolerance", small);
+
+    scalar nextTime = startTime;
+
+    // Make sure the first time in the selected times is less than the starting time
+    if (times.size())
+    {
+        forAll(times, ti)
+        {
+            if (times[ti].value() > nextTime+tolerance)
+            {
+                nextTime += dt;
+            }
+            else
+            {
+                break;
+            }
+
+
+        }
+    }
+
+    // Space times
+    forAll(times, ti)
+    {
+        if (times[ti].value() > endTime+tolerance)
+        {
+            break;
+        }
+        else if (times[ti].value() < startTime-tolerance)
+        {
+            continue;
+        }
+
+        scalar diff(times[ti].value() - nextTime);
+        if (mag(diff) < tolerance)
+        {
+            timesToKeep.append(times[ti]);
+            nextTime += dt;
+        }
+        else
+        {
+            prunedTimes.append(times[ti]);
+        }
+    }
+
+    times =
+        args.optionFound("invert")
+      ? prunedTimes
+      : timesToKeep;
+
+    if (args.optionFound("rm"))
+    {
+        if (args.optionFound("processor"))
+        {
+            for (label proci=0; proci<nProcs; proci++)
+            {
+                const fileName procPath
+                (
+                    args.path()/(word("processor") + name(proci))
+                );
+
+                forAll(times, ti)
+                {
+                    const fileName procTimePath
+                    (
+                        fileHandler().filePath(procPath/times[ti].name())
+                    );
+
+                    if (isDir(procTimePath))
+                    {
+                        rmDir(procTimePath);
+                    }
+                }
+            }
+        }
+        else
+        {
+            forAll(times, ti)
+            {
+                const fileName procTimePath
+                (
+                    fileHandler().filePath(args.path()/times[ti].name())
+                );
+
+                rmDir(procTimePath);
+            }
+        }
+    }
+    else
+    {
+        forAll(times, ti)
+        {
+            Info<< times[ti].name() << endl;
+        }
+    }
+
+    return 0;
+
+}
+
+
+// ************************************************************************* //
