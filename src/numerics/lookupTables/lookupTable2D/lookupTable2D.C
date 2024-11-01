8edc2bd8321963d45dd007ad855a6c5f07c816b0
Jeff Heylmun <heylmun@synthetik-technologies.com>
2024-11-01 11:55:48 -0500

    DEV: Added option to specify lookupTable dictionary name entry is only written as a dictionary if a name is provided

--- src/numerics/lookupTables/lookupTable2D/lookupTable2D.C
+++ src/numerics/lookupTables/lookupTable2D/lookupTable2D.C
@@ -795,9 +795,13 @@ void Foam::lookupTable2D<Type>::read
 }
 
 template<class Type>
-void  Foam::lookupTable2D<Type>::write(Ostream& os) const
+void  Foam::lookupTable2D<Type>::write(Ostream& os, const word& dictName) const
 {
-    os << nl << indent << token::BEGIN_BLOCK << nl << incrIndent;
+    if (!dictName.empty())
+    {
+        os  << indent << dictName << nl
+            << indent << token::BEGIN_BLOCK << nl << incrIndent;
+    }
 
     if (solver_.valid())
     {
@@ -835,7 +839,10 @@ void  Foam::lookupTable2D<Type>::write(Ostream& os) const
 
     os  << decrIndent << indent << token::END_BLOCK << endl;
 
-    os  << decrIndent << indent << token::END_BLOCK << endl;
+    if (!dictName.empty())
+    {
+        os  << decrIndent << indent << token::END_BLOCK << endl;
+    }
 }
 
 // * * * * * * * * * * * * * * * Member Operators  * * * * * * * * * * * * * //
@@ -860,9 +867,21 @@ void Foam::lookupTable2D<Type>::operator=(const lookupTable2D<Type>& table)
 // * * * * * * * * * * * * * * * IOstream Functions  * * * * * * * * * * * * //
 
 template<class Type>
-void  Foam::writeEntry(Ostream& os, const lookupTable2D<Type>& table)
+void Foam::writeEntry(Ostream& os, const lookupTable2D<Type>& table)
 {
     table.write(os);
+}
+
+
+template<class Type>
+void Foam::writeEntry
+(
+    Ostream& os,
+    const lookupTable2D<Type>& table,
+    const word& dictName
+)
+{
+    table.write(os, dictName);
 }
 
 
