0bb915934f7eaa1a9faad708a57d61e5976a6f17
Christopher Houthuysen <chouthuysen@gmail.com>
2025-03-18 12:26:09 -0400

    Added length to diameter ratio

--- src/diameterModels/diameterModel/diameterModel.C
+++ src/diameterModels/diameterModel/diameterModel.C
@@ -130,4 +130,14 @@ Foam::tmp<Foam::volScalarField> Foam::diameterModel::dMdt() const
     );
 }
 
+Foam::tmp<Foam::volScalarField> Foam::diameterModel::LDratio() const
+{
+    return volScalarField::New
+    (
+        IOobject::groupName("LDratio", d_.group()),
+        d_.mesh(),
+        dimensionedScalar(dimless, 1.0)
+    );
+}
+
 // ************************************************************************* //
