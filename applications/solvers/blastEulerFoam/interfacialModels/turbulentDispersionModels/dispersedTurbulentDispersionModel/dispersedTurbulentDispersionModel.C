8167cd44f7a4d739304cbf32a4f3ede1bd54ac03
Jeff Heylmun <heylmun@synthetik-technologies.com>
2025-05-02 06:32:11 -0500

    DEV: Added dispersedTurburbulentDispersionModel

---
+++ applications/solvers/blastEulerFoam/interfacialModels/turbulentDispersionModels/dispersedTurbulentDispersionModel/dispersedTurbulentDispersionModel.C
@@ -0,0 +1,80 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2014-2019 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+2017-05-18 Jeff Heylmun:    Added support of polydisperse phase models
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dispersedTurbulentDispersionModel.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace turbulentDispersionModels
+{
+    defineTypeNameAndDebug(dispersedTurbulentDispersionModel, 0);
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::turbulentDispersionModels::dispersedTurbulentDispersionModel::
+dispersedTurbulentDispersionModel
+(
+    const dictionary& dict,
+    const phasePair& pair
+)
+:
+    turbulentDispersionModel(dict, pair)
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::turbulentDispersionModels::dispersedTurbulentDispersionModel::
+~dispersedTurbulentDispersionModel()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+const Foam::phaseCompressible::momentumTransportModel&
+Foam::turbulentDispersionModels::dispersedTurbulentDispersionModel::
+continuousTurbulence() const
+{
+    return pair_.continuous().mesh().lookupObject
+    <
+        phaseCompressible::momentumTransportModel
+    >
+    (
+        IOobject::groupName
+        (
+            momentumTransportModel::typeName,
+            pair_.continuous().name()
+        )
+    );
+}
+
+// ************************************************************************* //
