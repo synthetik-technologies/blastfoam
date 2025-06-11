cb7db20d4c5e2baae9fa984eb7d462210ae8f87d
Jeff Heylmun <heylmun@synthetik-technologies.com>
2025-06-11 14:49:24 -0500

    BUG: Fixed enthalpy departure function

--- src/thermodynamicModels/specie/equationOfStates/equationOfStates/VanderWaals/VanderWaals.C
+++ src/thermodynamicModels/specie/equationOfStates/equationOfStates/VanderWaals/VanderWaals.C
@@ -1,12 +1,9 @@
 /*---------------------------------------------------------------------------*\
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
-     \\/     M anipulation  |
--------------------------------------------------------------------------------
-2020-04-02 Jeff Heylmun:    Modified class for a density based thermodynamic
-                            class
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2025
+     \\/     M anipulation  | Synthetik Applied Technologies
 -------------------------------------------------------------------------------
 License
     This file is derivative work of OpenFOAM.
